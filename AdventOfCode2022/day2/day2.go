b0ddb4831334c6fcf0f21c45e3b0be1d9f74bb92
hoduc <duc_ho90@yahoo.com>
2022-12-03 17:19:21 -0800

    [AdventOfCode2022][day2] part 2

--- AdventOfCode2022/day2/day2.go
+++ AdventOfCode2022/day2/day2.go
@@ -24,6 +24,19 @@
 // In this example, if you were to follow the strategy guide, you would get a total score of 15 (8 + 1 + 6).
 
 // What would your total score be if everything goes exactly according to your strategy guide?
+
+// --- Part Two ---
+// The Elf finishes helping with the tent and sneaks back over to you. "Anyway, the second column says how the round needs to end: X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win. Good luck!"
+
+// The total score is still calculated in the same way, but now you need to figure out what shape to choose so the round ends as indicated. The example above now goes like this:
+
+// In the first round, your opponent will choose Rock (A), and you need the round to end in a draw (Y), so you also choose Rock. This gives you a score of 1 + 3 = 4.
+// In the second round, your opponent will choose Paper (B), and you choose Rock so you lose (X) with a score of 1 + 0 = 1.
+// In the third round, you will defeat your opponent's Scissors with Rock for a score of 1 + 6 = 7.
+// Now that you're correctly decrypting the ultra top secret strategy guide, you would get a total score of 12.
+
+// Following the Elf's instructions for the second column, what would your total score be if everything goes exactly according to your strategy guide?
+
 package main
 
 import (
@@ -106,7 +119,9 @@ func inputToHand(input string) Hand {
 //go:embed day2.txt
 var day2txt string
 
-func part1() int {
+type onRightHandFn func(Hand, Hand) Hand
+
+func readInputAndCalculateScore(onRightHand onRightHandFn) int {
     totalScore := 0
     onLine := func(line string) error {
         if len(line) >  0 {
@@ -115,7 +130,7 @@ func part1() int {
             if left == WrongHand {
                 return errors.New(fmt.Sprintf("Error parsing hand with input [%v]", splits[0]))
             }
-            right := inputToHand(splits[1])
+            right := onRightHand(left, inputToHand(splits[1]))
             if right == WrongHand {
                 return errors.New(fmt.Sprintf("Error parsing hand with input [%v]", splits[1]))
             }
@@ -133,6 +148,39 @@ func part1() int {
     return totalScore
 }
 
+func onRightHandPart1(left Hand, right Hand) Hand {
+    return right
+}
+
+func onRightHandPart2(left Hand, right Hand) Hand {
+    switch right {
+    case Rock: // X: need to lose
+        switch left {
+        case Rock:
+            return Scissors
+        case Paper:
+            return Rock
+        case Scissors:
+            return Paper
+        }
+        return WrongHand
+    case Paper: // Y: need to draw
+        return left
+    case Scissors: // Z: need to win
+        switch left {
+        case Rock:
+            return Paper
+        case Paper:
+            return Scissors
+        case Scissors:
+            return Rock
+        }
+        return WrongHand
+    }
+    return WrongHand
+}
+
 func main() {
-    fmt.Printf("par1:%v\n", part1())
+    fmt.Printf("part1:%v\n", readInputAndCalculateScore(onRightHandPart1))
+    fmt.Printf("part2:%v\n", readInputAndCalculateScore(onRightHandPart2))
 }
