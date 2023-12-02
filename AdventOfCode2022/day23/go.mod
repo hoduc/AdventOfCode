2e6f8f510ec324ca4aacede3dab84725f501a905
hoduc <duc_ho90@yahoo.com>
2023-12-02 13:08:13 -0800

    [AdventOfCode2022][day23] part 1

---
+++ AdventOfCode2022/day23/go.mod
@@ -0,0 +1,7 @@
+module github.com/hoduc/AdventOfCode/AdventOfCode2022/day23
+
+go 1.19
+
+replace github.com/hoduc/AdventOfCode/AdventOfCode2022/util => ../util
+
+require github.com/hoduc/AdventOfCode/AdventOfCode2022/util v0.0.0-00010101000000-000000000000
--- AdventOfCode2022/util/util.go
+++ AdventOfCode2022/util/util.go
@@ -1,119 +1,142 @@
 package util
 
 import (
-    "os"
-    "log"
-    "bufio"
-    "strings"
-    "math"
-    "errors"
+	"bufio"
+	"errors"
+	"log"
+	"math"
+	"os"
+	"strings"
 )
 
 type onlineFn func(string) error
 
 func ReadLines(fileName string, onLine onlineFn) error {
-    f, err := os.OpenFile(fileName, os.O_RDONLY, os.ModePerm)
-    if err != nil {
-        log.Fatalf("Error open file: %v", err)
-        return err
-    }
+	f, err := os.OpenFile(fileName, os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		log.Fatalf("Error open file: %v", err)
+		return err
+	}
 
-    defer f.Close()
+	defer f.Close()
 
-    scanner := bufio.NewScanner(f)
-    for scanner.Scan() {
-        line := scanner.Text()
-        if err := onLine(line); err != nil {
-            log.Fatalf("onLine failed on [%v]", err)
-            return err
-        }
-    }
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if err := onLine(line); err != nil {
+			log.Fatalf("onLine failed on [%v]", err)
+			return err
+		}
+	}
 
-    if err := scanner.Err(); err != nil {
-        log.Fatalf("Scan file err: %v", err)
-        return err
-    }
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Scan file err: %v", err)
+		return err
+	}
 
-    return nil
+	return nil
 }
 
 func ReadLinesEmbed(lines string, onLine onlineFn) error {
-    scanner := bufio.NewScanner(strings.NewReader(lines))
-    for scanner.Scan() {
-        line := scanner.Text()
-        if err := onLine(line); err != nil {
-            log.Fatalf("onLine failed on [%v]", err)
-            return err
-        }
-    }
-    return nil
+	scanner := bufio.NewScanner(strings.NewReader(lines))
+	for scanner.Scan() {
+		line := scanner.Text()
+		if err := onLine(line); err != nil {
+			log.Fatalf("onLine failed on [%v]", err)
+			return err
+		}
+	}
+	return nil
 }
 
 func ReadLinesEmbedLineNumber(lines string, lineNumber int, onLine onlineFn) error {
-    log.Println("hello-there")
-    lineNo := 0
-    scanner := bufio.NewScanner(strings.NewReader(lines))
-    for scanner.Scan() {
-        line := scanner.Text()
-        if lineNo == lineNumber {
-            if err := onLine(line); err != nil {
-                log.Fatalf("onLine failed on [%v]", err)
-                return err
-            }
-            break
-        }
-        lineNo += 1
-    }
-    if lineNo < lineNumber {
-        log.Fatalf("Wrong lineNumber to read from file")
-        return errors.New("Wrong lineNumber to read from file")
-    }
-    return nil
+	log.Println("hello-there")
+	lineNo := 0
+	scanner := bufio.NewScanner(strings.NewReader(lines))
+	for scanner.Scan() {
+		line := scanner.Text()
+		if lineNo == lineNumber {
+			if err := onLine(line); err != nil {
+				log.Fatalf("onLine failed on [%v]", err)
+				return err
+			}
+			break
+		}
+		lineNo += 1
+	}
+	if lineNo < lineNumber {
+		log.Fatalf("Wrong lineNumber to read from file")
+		return errors.New("Wrong lineNumber to read from file")
+	}
+	return nil
 }
 
 // TODO: More type
 func Max(a, b interface{}) interface{} {
-    switch a.(type) {
-    case int:
-        return int(MaxInt(int64(a.(int)), int64(b.(int))))
-    case int32:
-        return int32(MaxInt(int64(a.(int32)), int64(b.(int32))))
-    case int64:
-        return MaxInt(a.(int64), b.(int64))
-    case float32:
-        return float32(math.Max(float64(a.(float32)), float64(b.(float32))))
-    case float64:
-        return math.Max(a.(float64), b.(float64))
-    }
-    return nil
+	switch a.(type) {
+	case int:
+		return int(MaxInt(int64(a.(int)), int64(b.(int))))
+	case int32:
+		return int32(MaxInt(int64(a.(int32)), int64(b.(int32))))
+	case int64:
+		return MaxInt(a.(int64), b.(int64))
+	case float32:
+		return float32(math.Max(float64(a.(float32)), float64(b.(float32))))
+	case float64:
+		return math.Max(a.(float64), b.(float64))
+	}
+	return nil
 }
 
 func MaxInt(a, b int64) int64 {
-    if a < b {
-        return b
-    }
-    return a
+	if a < b {
+		return b
+	}
+	return a
 }
 
-func Abs(x interface{}) interface {}{
-    switch x.(type) {
-    case int:
-        return int(AbsInt(int64(x.(int))))
-    case int32:
-        return int32(AbsInt(int64(x.(int32))))
-    case int64:
-        return AbsInt(x.(int64))
-    case float32:
-        return float32(math.Abs(float64(x.(float32))))
-    case float64:
-        return math.Abs(x.(float64))
-    }
-    return nil
+func Min(a, b interface{}) interface{} {
+	switch a.(type) {
+	case int:
+		return int(MinInt(int64(a.(int)), int64(b.(int))))
+	case int32:
+		return int32(MinInt(int64(a.(int32)), int64(b.(int32))))
+	case int64:
+		return MinInt(a.(int64), b.(int64))
+	case float32:
+		return float32(math.Min(float64(a.(float32)), float64(b.(float32))))
+	case float64:
+		return math.Min(a.(float64), b.(float64))
+	}
+	return nil
+}
+
+func MinInt(a, b int64) int64 {
+	if a < b {
+		return a
+	}
+	return a
+}
+
+func Abs(x interface{}) interface{} {
+	switch x.(type) {
+	case int:
+		return int(AbsInt(int64(x.(int))))
+	case int32:
+		return int32(AbsInt(int64(x.(int32))))
+	case int64:
+		return AbsInt(x.(int64))
+	case float32:
+		return float32(math.Abs(float64(x.(float32))))
+	case float64:
+		return math.Abs(x.(float64))
+	}
+	return nil
 }
 
 func AbsInt(x int64) int64 {
-    if (x < 0) {
-        return -x
-    }
-    return x
+	if x < 0 {
+		return -x
+	}
+	return x
 }
