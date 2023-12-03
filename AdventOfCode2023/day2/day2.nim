cf9ff5a95f94f9a682abbcf1e527d98a709d4803
hoduc <duc_ho90@yahoo.com>
2023-12-02 21:37:59 -0800

    [AdventOfCode2023][day2][part2]

--- AdventOfCode2023/day2/day2.nim
+++ AdventOfCode2023/day2/day2.nim
@@ -26,14 +26,39 @@ The Elf would first like to know which games would have been possible if the bag
 In the example above, games 1, 2, and 5 would have been possible if the bag had been loaded with that configuration. However, game 3 would have been impossible because at one point the Elf showed you 20 red cubes at once; similarly, game 4 would also have been impossible because the Elf showed you 15 blue cubes at once. If you add up the IDs of the games that would have been possible, you get 8.
 
 Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?
+
+--- Part Two ---
+
+The Elf says they've stopped producing snow because they aren't getting any water! He isn't sure why the water stopped; however, he can show you how to get to the water source to check it out for yourself. It's just up ahead!
+
+As you continue your walk, the Elf poses a second question: in each game you played, what is the fewest number of cubes of each color that could have been in the bag to make the game possible?
+
+Again consider the example games from earlier:
+
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+
+    In game 1, the game could have been played with as few as 4 red, 2 green, and 6 blue cubes. If any color had even one fewer cube, the game would have been impossible.
+    Game 2 could have been played with a minimum of 1 red, 3 green, and 4 blue cubes.
+    Game 3 must have been played with at least 20 red, 13 green, and 6 blue cubes.
+    Game 4 required at least 14 red, 3 green, and 15 blue cubes.
+    Game 5 needed no fewer than 6 red, 3 green, and 2 blue cubes in the bag.
+
+The power of a set of cubes is equal to the numbers of red, green, and blue cubes multiplied together. The power of the minimum set of cubes in game 1 is 48. In games 2-5 it was 12, 1560, 630, and 36, respectively. Adding up these five powers produces the sum 2286.
+
+For each game, find the minimum set of cubes that must have been present. What is the sum of the power of these sets?
+
 ]#
 
 import strutils
 import unittest
+import sugar # ->
 
-# 12 red cubes, 13 green cubes, and 14 blue cubes?
-proc day2(fileName: string): int =
-    var possibleGameIdSum = 0
+proc day2(fileName: string, partFn: (int, openarray[string]) -> int): int =
+    var sum = 0
     for line in lines(fileName):
         let gameSplit = line.split(":")
         let game = gameSplit[1]
@@ -43,36 +68,68 @@ proc day2(fileName: string): int =
         let gameSet = game.split(";")
         # echo "==="
         # echo line
-        var playAble = true
-        for gs in gameSet:
-            var
-                red = 12
-                green = 13
-                blue = 14
-            let cubes = gs.split(",")
-            # echo "cubes:", cubes
-            for cube in cubes:
-                let cs = cube.split(" ")
-                let
-                    count = parseInt(cs[1])
-                    color = cs[2]
-                if color == "red":
-                    red -= count
-                elif color == "green":
-                    green -= count
-                elif color == "blue":
-                    blue -= count
-                # echo "| red:", red, "| green:", green, "| blue:", blue
-                if red < 0 or green < 0 or blue < 0:
-                    playAble = false
-                    break
-        if playAble:
-            # echo "Y!!!"
-            possibleGameIdSum += gameId
-    return possibleGameIdSum
+        sum += partFn(gameId, gameSet)
+    return sum
+        
     
 
+# 12 red cubes, 13 green cubes, and 14 blue cubes?
+proc day2Part1(gameId: int, gameSet: openarray[string]): int =
+    var playAble = true
+    for gs in gameSet:
+        var
+            red = 12
+            green = 13
+            blue = 14
+        let cubes = gs.split(",")
+        # echo "cubes:", cubes
+        for cube in cubes:
+            let cs = cube.split(" ")
+            let
+                count = parseInt(cs[1])
+                color = cs[2]
+            if color == "red":
+                red -= count
+            elif color == "green":
+                green -= count
+            elif color == "blue":
+                blue -= count
+            # echo "| red:", red, "| green:", green, "| blue:", blue
+            if red < 0 or green < 0 or blue < 0:
+                playAble = false
+                break
+    if playAble:
+        # echo "Y!!!"
+        return gameId
+    return 0
+
+proc day2Part2(gameId: int, gameSet: openarray[string]): int =
+    var
+        red = -1
+        green = -1
+        blue = -1
+    for gs in gameSet:
+        let cubes = gs.split(",")
+        # echo "cubes:", cubes
+        for cube in cubes:
+            let cs = cube.split(" ")
+            let
+                count = parseInt(cs[1])
+                color = cs[2]
+            if color == "red":
+                red = if (red == -1): count else: max(red, count)
+            elif color == "green":
+                green = if (green == -1): count else: max(green, count)
+            elif color == "blue":
+                blue = if (blue == -1): count else: max(blue, count)
+    # echo "game:", gameId, "(", red, ",", green, ",", blue, ")" , "=>", red*green*blue
+    return red*green*blue    
+
 test "day2_sample.txt":
-    check day2("day2_sample.txt") == 8
+    check day2("day2_sample.txt", day2Part1) == 8
 test "day2_1.txt":
-    check day2("day2_1.txt") == 2795
+    check day2("day2_1.txt", day2Part1) == 2795
+test "day2_2_sample.txt":
+    check day2("day2_sample.txt", day2Part2) == 2286
+test "day2_2.txt":
+    check day2("day2_1.txt", day2Part2) == 75561
