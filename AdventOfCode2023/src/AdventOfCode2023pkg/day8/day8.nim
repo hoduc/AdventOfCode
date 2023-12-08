0e7a7bb92bc44112e1f37cc3bfe0b64451b2d95e
hoduc <duc_ho90@yahoo.com>
2023-12-08 09:25:02 -0800

    day 8 part 2 inprogress

--- AdventOfCode2023/src/AdventOfCode2023pkg/day8/day8.nim
+++ AdventOfCode2023/src/AdventOfCode2023pkg/day8/day8.nim
@@ -34,6 +34,41 @@ ZZZ = (ZZZ, ZZZ)
 
 Starting at AAA, follow the left/right instructions. How many steps are required to reach ZZZ?
 
+--- Part Two ---
+
+The sandstorm is upon you and you aren't any closer to escaping the wasteland. You had the camel follow the instructions, but you've barely left your starting position. It's going to take significantly more steps to escape!
+
+What if the map isn't for people - what if the map is for ghosts? Are ghosts even bound by the laws of spacetime? Only one way to find out.
+
+After examining the maps a bit longer, your attention is drawn to a curious fact: the number of nodes with names ending in A is equal to the number ending in Z! If you were a ghost, you'd probably just start at every node that ends with A and follow all of the paths at the same time until they all simultaneously end up at nodes that end with Z.
+
+For example:
+
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+
+Here, there are two starting nodes, 11A and 22A (because they both end with A). As you follow each left/right instruction, use that instruction to simultaneously navigate away from both nodes you're currently on. Repeat this process until all of the nodes you're currently on end with Z. (If only some of the nodes you're on end with Z, they act like any other node and you continue as normal.) In this example, you would proceed as follows:
+
+    Step 0: You are at 11A and 22A.
+    Step 1: You choose all of the left paths, leading you to 11B and 22B.
+    Step 2: You choose all of the right paths, leading you to 11Z and 22C.
+    Step 3: You choose all of the left paths, leading you to 11B and 22Z.
+    Step 4: You choose all of the right paths, leading you to 11Z and 22B.
+    Step 5: You choose all of the left paths, leading you to 11B and 22C.
+    Step 6: You choose all of the right paths, leading you to 11Z and 22Z.
+
+So, in this example, you end up entirely on nodes that end in Z after 6 steps.
+
+Simultaneously start on every node that ends with A. How many steps does it take before you're only on nodes that end with Z?
+
 ]#
 
 import std/tables
@@ -41,7 +76,7 @@ import std/strutils
 import std/sequtils
 import std/sugar
 
-proc day8*(fileName: string): int =
+proc instructionsGraph(fileName: string): (string, Table[string, seq[string]]) =
     var
         instructions = ""
         g = initTable[string, seq[string]]()
@@ -60,17 +95,41 @@ proc day8*(fileName: string): int =
             for node in strip(nodes[1])[1..^2].split(",").map(x => strip(x)):
                 g[fromNode].add(node)
         lineCount += 1
-    
-    echo instructions
-    echo g
 
-    var i = 0
-    var current = ("AAA", 0)
-    while current[0] != "ZZZ":
-        let instruction = instructions[i mod len(instructions)]
+    return (instructions, g)
+
+proc explore(g:Table[string, seq[string]],
+             instructions: string,
+             currents: var seq[string]): int =
+    var step = 0
+    var simFactor = len(currents) # how many to simutaneously start exploring
+    var sim = 0
+    while sim != simFactor:
+        sim = 0
+        let instruction = instructions[step mod len(instructions)]
         var nextNodeIndex = 0
         if instruction == 'R':
             nextNodeIndex = 1
-        current = (g[current[0]][nextNodeIndex], current[1] + 1)
-        i += 1
-    return current[1]
+        for _ in 0 ..< simFactor:
+            let newNode = g[currents[0]][nextNodeIndex]
+            if newNode[^1] == 'Z':
+                sim += 1
+            currents.add(newNode)
+            currents.delete(0)
+        echo(sim, ",", step, "," , currents)
+        step += 1
+    return step
+
+proc day81*(fileName: string): int =
+    var (instructions, g) = instructionsGraph(fileName)
+    var currents = @["AAA"]
+    return explore(g, instructions, currents)
+
+proc day82*(fileName: string): int =
+    var (instructions, g) = instructionsGraph(fileName)
+    var currents: seq[string] = @[]
+    for k, v in g:
+        if k[^1] == 'A':
+            currents.add(k)
+    echo currents
+    return explore(g, instructions, currents)
