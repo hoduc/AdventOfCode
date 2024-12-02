471cb7248832b789459bc17ab76d02c684b25718
hoduc <duc_ho90@yahoo.com>
2024-12-01 20:34:27 -0800

    [python][Day1] part 2

--- AdventOfCode2024/python/day1/day1.py
+++ AdventOfCode2024/python/day1/day1.py
@@ -42,7 +42,42 @@ To find the total distance between the left list and the right list, add up the 
 
 Your actual left and right lists contain many location IDs. What is the total distance between your lists?
 
+--- Part Two ---
+
+Your analysis only confirmed what everyone feared: the two lists of location IDs are indeed very different.
+
+Or are they?
+
+The Historians can't agree on which group made the mistakes or how to read most of the Chief's handwriting, but in the commotion you notice an interesting detail: a lot of location IDs appear in both lists! Maybe the other numbers aren't location IDs at all but rather misinterpreted handwriting.
+
+This time, you'll need to figure out exactly how often each number from the left list appears in the right list. Calculate a total similarity score by adding up each number in the left list after multiplying it by the number of times that number appears in the right list.
+
+Here are the same example lists again:
+
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+
+For these example lists, here is the process of finding the similarity score:
+
+    The first number in the left list is 3. It appears in the right list three times, so the similarity score increases by 3 * 3 = 9.
+    The second number in the left list is 4. It appears in the right list once, so the similarity score increases by 4 * 1 = 4.
+    The third number in the left list is 2. It does not appear in the right list, so the similarity score does not increase (2 * 0 = 0).
+    The fourth number, 1, also does not appear in the right list.
+    The fifth number, 3, appears in the right list three times; the similarity score increases by 9.
+    The last number, 3, appears in the right list three times; the similarity score again increases by 9.
+
+So, for these example lists, the similarity score at the end of this process is 31 (9 + 4 + 0 + 0 + 9 + 9).
+
+Once again consider your left and right lists. What is their similarity score?
+
+
 """
+from collections import Counter
+
 def read_file_to_lr(file_name):
     l, r = [], []
     with open(file_name) as f:
@@ -53,10 +88,15 @@ def read_file_to_lr(file_name):
     return (l, r)
 
 
-def part1():
+def read_file_to_lr_sorted():
     l, r = read_file_to_lr('day1.txt')
-    l = sorted(l)
-    r = sorted(r)
+    return sorted(l), sorted(r)
+
+def part1():
+    l, r = read_file_to_lr_sorted()    
     return sum([abs(l[i] - r[i]) for i in range(len(l))])
 
-print(part1())
+def part2():
+    l, r = read_file_to_lr_sorted()
+    r = Counter(r)
+    return sum([e *r.get(e, 0) for e in l])
