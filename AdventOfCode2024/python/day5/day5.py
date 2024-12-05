c590301ccd13b82c316990591ff55ddb1412f8df
hoduc <duc_ho90@yahoo.com>
2024-12-05 11:06:10 -0800

    [python][day5] part2

--- AdventOfCode2024/python/day5/day5.py
+++ AdventOfCode2024/python/day5/day5.py
@@ -79,17 +79,36 @@ Of course, you'll need to be careful: the actual list of page ordering rules is 
 
 Determine which updates are already in the correct order. What do you get if you add up the middle page number from those correctly-ordered updates?
 
+--- Part Two ---
+
+While the Elves get to work printing the correctly-ordered updates, you have a little time to fix the rest of them.
+
+For each of the incorrectly-ordered updates, use the page ordering rules to put the page numbers in the right order. For the above example, here are the three incorrectly-ordered updates and their correct orderings:
+
+    75,97,47,61,53 becomes 97,75,47,61,53.
+    61,13,29 becomes 61,29,13.
+    97,13,75,29,47 becomes 97,75,47,29,13.
+
+After taking only the incorrectly-ordered updates and ordering them correctly, their middle page numbers are 47, 29, and 47. Adding these together produces 123.
+
+Find the updates which are not in the correct order. What do you get if you add up the middle page numbers after correctly ordering just those updates?
+
+
 """
 
 from collections import OrderedDict, defaultdict
 from functools import cmp_to_key
 
-## LETS PARSE THE INPUT INTO MAP FIRST
-def part(file_name):
+def part1_sum_fn(sort_updates, updates):
+    return sort_updates == updates
+
+def part2_sum_fn(sort_updates, updates):
+    return sort_updates != updates
+    
+def part(file_name, should_consider_sum_fn):
     middle_page_sum = 0
     update_mode = False
     rules = defaultdict(set)
-    correct_updates = []
     def cmp(a, b):
         if b in rules[a]:
             return -1
@@ -108,7 +127,7 @@ def part(file_name):
                     rules[line_split[0]].add(line_split[-1])
                 else:
                     updates = [int(e) for e in line.split(",")]
-                    print("===", updates)
+                    # print("===", updates)
                     # determine if the update is in the right order according to rule
                     # each element should be before other in the correct order
                     # LETS DO THE BRUTE FORCE SOLUTION FIRST
@@ -119,23 +138,28 @@ def part(file_name):
                     # DOES THAT MEANS THAT IF SORT THE UPDATE BASE ON THE RULE ABOVE AND IF IT COMES OUT TO BE THE SAME ARRAY
                     # THEN THAT MEANS IT CORRECT ORDER ELSE IT IS NOT. LETS TRY THAT
                     sort_updates = sorted(updates, key=cmp_to_key(cmp))
-                    print("-->", sort_updates)
-                    if sort_updates == updates:
-                        print("===correct")
-                        middle_page_sum += updates[len(updates) // 2]
+                    # print("-->", sort_updates)
+                    if should_consider_sum_fn(sort_updates, updates):
+                        # print("===sumFnTrue:", sort_updates[len(sort_updates) // 2])
+                        middle_page_sum += sort_updates[len(sort_updates) // 2]
                         continue
-                    print("===NOPE")
+                    # print("===NOPE")
                     
             else:
                 update_mode = True
     return middle_page_sum
 
-assert part("day5_example.txt") == 143
+assert part("day5_example.txt", part1_sum_fn) == 143
+assert part("day5_example.txt", part2_sum_fn) == 123
+
+
 
 def part1():
-    return part("day5.txt")
+    return part("day5.txt", part1_sum_fn)
+
+def part2():
+    return part("day5.txt", part2_sum_fn)
 
 
 print(part1()) # 6242
-
-# LETS GIT COMMIT THIS
+print(part2()) # 5169
